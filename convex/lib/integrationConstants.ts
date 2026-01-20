040e29d916a510040ad4ec9bfca2cb85a7617d28
Chris Hayes <christopherhayes@gmail.com>
2026-01-20 12:54:17 -0500

    feat(gmail): add Gmail provider constants and scopes

--- convex/lib/integrationConstants.ts
+++ convex/lib/integrationConstants.ts
@@ -6,3 +6,15 @@ export const GOOGLE_CALENDAR_SCOPES = [
   'https://www.googleapis.com/auth/calendar',
   'https://www.googleapis.com/auth/calendar.events',
 ]
+
+export const GMAIL_PROVIDER = 'gmail'
+
+// Scopes for each permission level
+export const GMAIL_SCOPES_READ = ['https://www.googleapis.com/auth/gmail.readonly']
+
+export const GMAIL_SCOPES_READ_DRAFT = [
+  'https://www.googleapis.com/auth/gmail.readonly',
+  'https://www.googleapis.com/auth/gmail.compose',
+]
+
+export const GMAIL_SCOPES_FULL = ['https://www.googleapis.com/auth/gmail.modify']
